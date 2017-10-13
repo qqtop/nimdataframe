dd2116a1265aa455c76abf8cb5fc06e949f1f180
qqtop <pict100@gmail.com>
2017-10-13 10:09:59 +0800

    Tiny improvements

--- nimdataframe/examples/nimdfT9.nim
+++ nimdataframe/examples/nimdfT9.nim
@@ -11,7 +11,7 @@ import nimcx,nimdataframe,algorithm,stats
 #  nimble install nimcx
 #  nimble install https://github.com/qqtop/nimdataframe.git
 #  
-#  2017-10-07
+#  2017-10-13
 #  
 
 var dmaxrows = 3000        # any number larger than actual row count is fine 
@@ -21,21 +21,9 @@ var dndf:nimdf             # define a nim dataframe
 dndf = createDataFrame(ddata,cols = 5,rows=dmaxrows)  # specify desired cols as per data file , default = 2 
 printLnBiCol("Data Source : " & ddata)
 # display the dataframe
-showDf(dndf, rows = 15,cols = @[1,2,3,4,5],colwd = @[10,10,10,10,10], colcolors = @[pastelgreen,pastelpink],showframe = true,framecolor = goldenrod,showHeader = false,leftalignflag = false) 
+var displaycols = @[1,2,3,4,5]
+showDf(dndf, rows = 15,cols = displaycols,colwd = @[10,10,10,10,10], colcolors = @[pastelgreen,pastelpink],showframe = true,framecolor = goldenrod,showHeader = false,leftalignflag = false) 
 echo()
 showDataframeInfo(dndf)  # show some information about dataframe
-printLn("Statistics  ",peru)
-let desiredcols = @[1,2,3,4,5]  # we want stats for all columns  or just state the ones you need
-var mydfstats = dfColumnStats(dndf,desiredcols)
-var xpos = 1
-curup(3)
-for x in 0..<mydfstats.len:
-   printLnBiCol("Column : " & $(desiredcols[x]) & " Statistics",xpos = xpos,styled={styleUnderscore})
-   showStats(mydfstats[x],xpos = xpos) 
-   xpos += 25
-   curup(15)
-curdn(18)   
-printLnBiCol("Statistics -> Rows processed: " & $getRowcount(dndf))
-printLnBiCol("Statistics -> Cols processed: " & $desiredcols.len & " of " & $getColcount(dndf))   
-
+dfShowColumnStats(dndf,displaycols)
 doFinish()
